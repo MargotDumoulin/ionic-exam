707050d5503f30c97818931fc812c8469cebb446
Snorloux <margot.dumoulin18@gmail.com>
2021-03-17 23:37:30 +0100

    :construction: Working on forms

--- src/pages/champions-list/champions-new/champions-new.ts
+++ src/pages/champions-list/champions-new/champions-new.ts
@@ -1,6 +1,8 @@
+import { Champion } from './../../../app/types.d';
 import { ChampionsProvider } from './../../../providers/champions/champions';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
 
 @IonicPage()
 @Component({
@@ -9,40 +11,49 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class ChampionsNewPage {
 
-  public champion: any = {
-    title: null,
-    country: null,
-    onAir: false,
-    pictureLink: null,
-    productor: null,
-    realisator: null,
-    releaseDate: null,
-    scenario: null,
-    synopsis: null,
-    time: null,
-    music: null
+  public championForm: FormGroup;
+
+  public champion: Champion = {
+    isWomen: null,
+    image: null,
+    name: null,
+    passive: null,
+    age: null,
+    skillQ: null,
+    skillE: null,
+    skillW: null,
+    skillR: null
   };
 
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
-    private Champion: ChampionsProvider
-  ) {}
+    private Champion: ChampionsProvider,
+    private formBuilder: FormBuilder
+  ) {
+    this.championForm = this.formBuilder.group({
+      isWomen: ['', Validators.required],
+      name: ['', Validators.required],
+      skillQ: ['', Validators.required],
+      skillW: ['', Validators.required],
+      skillE: ['', Validators.required],
+      skillR: ['', Validators.required],
+      age: ['', Validators.required]
+    })
+  }
 
   onAdd() {
     this.Champion.saveNewChampion(this.champion).subscribe(() => {
       this.champion = {
-        title: null,
-        country: null,
-        onAir: false,
-        pictureLink: null,
-        productor: null,
-        realisator: null,
-        releaseDate: null,
-        scenario: null,
-        synopsis: null,
-        time: null,
-        music: null
+        isWomen: null,
+        image: null,
+        name: null,
+        passive: null,
+        age: null,
+        skillQ: null,
+        skillE: null,
+        skillW: null,
+        skillR: null
       };
 
       this.navCtrl.pop();
